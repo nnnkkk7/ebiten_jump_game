c23023b9b73068b959247e3a48bc2e0d91f8a5b3
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-06-19 22:41:42 +0900

    スペースキーで動くように

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"image/color"
 	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 var img *ebiten.Image
@@ -13,6 +15,16 @@ var img *ebiten.Image
 const (
 	screenWidth  = 640
 	screenHeight = 480
+	baseX        = 100
+	basey        = 100
+	jumpingPower = 15
+	gravity      = 1
+)
+
+const (
+	modeStart = iota
+	modeGame
+	modeGameover
 )
 
 func init() {
@@ -25,15 +37,57 @@ func init() {
 }
 
 type Game struct {
+	mode  int
+	count int
+	score int
+	ebix  int
+	ebiy  int
+	gy    int
+}
+
+func NewGame() *Game {
+	g := &Game{}
+	g.init()
+	return g
+}
+func (g *Game) init() {
+	g.count = 0
+	g.score = 0
+	g.ebix = baseX
+	g.ebiy = basey
+	g.gy = 0
 }
 
 func (g *Game) Update() error {
+	switch g.mode {
+	case modeStart:
+		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+			g.mode = modeGame
+		}
+	case modeGame:
+		g.count++
+		g.score = g.count / 5
+		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+			g.gy -= jumpingPower
+			g.ebiy += g.gy
+			g.gy += gravity
+		}
 
+	case modeGameover:
+		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+			g.init()
+			g.mode = modeGame
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	screen.Fill(color.White)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(baseX, float64(g.ebiy))
+	op.Filter = ebiten.FilterLinear
+	screen.DrawImage(img, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -41,9 +95,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	g := &Game{}
-
-	if err := ebiten.RunGame(g); err != nil {
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
